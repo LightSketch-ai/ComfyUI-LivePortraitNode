1537227a7c3647b4b559f1e9deeddc47eed73fc2
Benjamin Irwin <benjamintdirwin@gmail.com>
2024-07-12 11:20:39 +0100

    Clean up libs

--- liveportrait/nodes.py
+++ liveportrait/nodes.py
@@ -1,26 +1,9 @@
 import os
-import sys
-import json
-import subprocess
-import numpy as np
-import re
-import datetime
-from typing import List
-import torch
-from PIL import Image, ExifTags
-from PIL.PngImagePlugin import PngInfo
-from pathlib import Path
-from string import Template
-import itertools
 
 import folder_paths
-from .logger import logger
+
 from .image_latent_nodes import *
-from .load_video_nodes import LoadVideoUpload, LoadVideoPath
-from .load_images_nodes import LoadImagesFromDirectoryUpload, LoadImagesFromDirectoryPath
-from .batched_nodes import VAEEncodeBatched, VAEDecodeBatched
-from .utils import ffmpeg_path, get_audio, hash_path, validate_path, requeue_workflow, gifski_path, calculate_file_hash, strip_path
-from comfy.utils import ProgressBar
+from .load_video_nodes import LoadVideoUpload
 
 folder_paths.folder_names_and_paths["VHS_video_formats"] = (
     [
