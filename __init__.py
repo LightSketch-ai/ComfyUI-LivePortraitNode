eac798153b9ae9fe8131a5f3be3333e41a564a90
Benjamin Irwin <benjamintdirwin@gmail.com>
2024-07-12 11:43:06 +0100

    Clean up libs

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
-from .liveportrait.nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+from .videohelpersuite.nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 import folder_paths
-from .liveportrait.server import server
+from .videohelpersuite.server import server
 
 WEB_DIRECTORY = "./web"
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "WEB_DIRECTORY"]
